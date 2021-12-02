cba396c10df09fc2c6b6db570128a97d69da0bf6
Loo Zheng Yuan <loozhengyuan@gmail.com>
2021-12-03 00:49:48 +0800

    feat: add day 2 solutions

---
+++ d02/p2/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func run() error {
+	// Read input from file
+	f, err := os.Open("d02/input.txt")
+	if err != nil {
+		return fmt.Errorf("open file: %w", err)
+	}
+	defer f.Close()
+
+	// Scan each line
+	var horizontal, depth, aim int
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		// Parse command and value
+		line := strings.Fields(s.Text())
+		if len(line) != 2 {
+			return fmt.Errorf("parse line: invalid format: %#v", line)
+		}
+		cmd := line[0]
+		val, err := strconv.Atoi(line[1])
+		if err != nil {
+			return fmt.Errorf("parse input: %#v: %w", val, err)
+		}
+
+		// Increment/decrement position counters
+		switch cmd {
+		case "forward":
+			horizontal += val
+			depth += aim * val
+		case "down":
+			aim += val
+		case "up":
+			aim -= val
+		default:
+			return fmt.Errorf("unexpected command: %v", cmd)
+		}
+	}
+	if err := s.Err(); err != nil {
+		return fmt.Errorf("scan input: %w", err)
+	}
+
+	fmt.Println(horizontal * depth)
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
+}
